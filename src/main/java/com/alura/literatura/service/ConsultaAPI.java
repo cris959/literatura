cbb58b6a00420357cd2e4a378982240760a48dda
cris959 <christiangaray959@gmail.com>
2025-12-16 10:40:23 -0300

    construyendo solicitud API

---
+++ src/main/java/com/alura/literatura/service/ConsultaAPI.java
@@ -0,0 +1,46 @@
+package com.alura.literatura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaAPI {
+
+    private final HttpClient client;
+
+
+    public ConsultaAPI() {
+        this.client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+    }
+
+    public String getData(String url) {
+        HttpRequest request = createRequest(url);
+        return sendRequest(request);
+    }
+
+    private HttpRequest createRequest(String url) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+    }
+
+    private String sendRequest(HttpRequest request) {
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            validateResponse(response);
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Error al obtener datos de la API", e);
+        }
+    }
+
+    private void validateResponse(HttpResponse<String> response) {
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Error en la solicitud HTTP: " + response.statusCode());
+        }
+    }
+}
